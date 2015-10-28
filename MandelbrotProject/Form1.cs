8a606705255033037dc4238ea1226dffcee1ca37
Jim-Alexander Berger Gundersen <alexandergundersen@yahoo.no>
2015-10-29 00:00:19 +0100

    Renders mandelbrot in color

--- MandelbrotProject/Form1.cs
+++ MandelbrotProject/Form1.cs
@@ -12,6 +12,37 @@ namespace MandelbrotProject
 {
     public partial class Form1 : Form
     {
+        double fitInRRange(int x, int width, double minR, double maxR)
+        {
+            double range = maxR - minR;
+
+            return x * (range / width) + minR;
+        }
+
+        double fitInIRange(int y, int width, double minI, double maxI)
+        {
+            double range = maxI - minI;
+
+            return y * (range / width) + minI; ;
+        }
+
+        int findMandelbrot(double cr, double ci, int maxN)
+        {
+            int i = 0;
+            double zr = 0, zi = 0;
+
+            while (i < maxN && zr * zr + zi * zi < 4.0)
+            {
+                double temp = zr * zr - zi * zi + cr;
+                zi = 2.0 * zr * zi + ci;
+                zr = temp;
+
+                i++;
+
+            }
+            return i;
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -19,10 +50,45 @@ namespace MandelbrotProject
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Point currentPoint = new Point(10, 10);
-            for (int i = 0; i < Renderer.Height; i++)
+            Renderer.CreateGraphics().Clear(Color.White);
+
+            /*SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+            for (int y = 0; y < Renderer.Height; y++)
             {
-                Renderer.CreateGraphics().FillRectangle(new SolidBrush(Color.Violet), 0+i, 0+i, 1, 1);
+                for (int x = 0; x < Renderer.Width; x++)
+                {
+                   
+                    Renderer.CreateGraphics().FillRectangle(whiteBrush, x, y, 1, 1);
+                    
+                }
+            }*/
+
+
+            double minR = System.Convert.ToDouble(MinR.Value);
+            double maxR = System.Convert.ToDouble(MaxR.Value);
+            double minI = System.Convert.ToDouble(MinI.Value);
+            double maxI = System.Convert.ToDouble(MaxI.Value);
+            int maxN = System.Convert.ToInt32(Iterations.Value);
+
+
+            for (int y = 0; y < Renderer.Height; y++)
+            {
+                for (int x = 0; x < Renderer.Width; x++)
+                {
+
+                    double cr = fitInRRange(x, Renderer.Width, minR, maxR);
+                    double ci = fitInIRange(y, Renderer.Height, minI, maxI);
+
+                    int n = findMandelbrot(cr, ci, maxN);
+
+                    double t = ((n + 0.0) / (maxN + 0.0));
+
+                    SolidBrush MandelColor = new SolidBrush(Color.FromArgb(System.Convert.ToInt32(9 * (1 - t) * t * t * t * 255), System.Convert.ToInt32(15 * (1 - t) * (1 - t) * t * t * 255), System.Convert.ToInt32(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255)));
+
+                    Renderer.CreateGraphics().FillRectangle(MandelColor, x, y, 1, 1);
+                    
+                }
             }
         }
 
