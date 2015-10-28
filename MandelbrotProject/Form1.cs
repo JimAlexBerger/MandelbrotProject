a58325dfc8b269f0ebfa24bfc0db721893a68389
Jim-Alexander Berger Gundersen <alexandergundersen@yahoo.no>
2015-10-28 18:10:19 +0100

    Forgot space before height and iterations

--- MandelbrotProject/Form1.cs
+++ MandelbrotProject/Form1.cs
@@ -19,7 +19,7 @@ namespace MandelbrotProject
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Width: " + WidthInput.Text +"Height: " + HeightInput.Text + "Iterations: " + IterationsInput.Text);
+            MessageBox.Show("Width: " + WidthInput.Text +" Height: " + HeightInput.Text + " Iterations: " + IterationsInput.Text);
         }
     }
 }
