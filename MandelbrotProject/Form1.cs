ceffbe3218aa834a849ce113ea3b8ca32b775df0
Jim-Alexander Berger Gundersen <alexandergundersen@yahoo.no>
2015-10-28 17:16:40 +0100

    Added a button to the form

--- MandelbrotProject/Form1.cs
+++ MandelbrotProject/Form1.cs
@@ -16,5 +16,10 @@ namespace MandelbrotProject
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button is pressed");
+        }
     }
 }
